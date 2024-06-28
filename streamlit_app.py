42b2d935f686a37de42a41fe43ce0adb25503454
RNBHR <febrian.bahariadi@gmail.com>
2024-06-28 16:11:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,17 @@ import pandas as pd
 
 # baca dataframe dari file csv 
 df_house_clean = pd.read_csv('csv/house_clean.csv')
+
+def main() : 
+    st.header('Halaman Streamlit Febrian Bahari Adi')
+    st.subheader('This is SubHeader')
+    st.markdown('# Rendering Markdown ')
+    st.write('Some Phytagorean Equation : ')
+    st.latex('c^2 = a^2+b^2')
+    st.write('Contoh dataframe')
+    st.dataframe(df_house_clean)
+    st.write('Metrics')
+    st.metric(label="Temperature", value="70 °F", delta="-1.2 °F")
+
+if __name__ == '__main__' : 
+    main()
